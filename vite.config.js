d29491564f1eb7f02ce639c5f88ecae9a0e6794b
Walker <walker310407@gmail.com>
2025-07-29 01:01:14 +0530

    hello

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(),tailwindcss()],
-})
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: "::",
+    port: 5173,
+  },
+  plugins: [
+    react(),
+    tailwindcss(),
+    mode === "development" && componentTagger(),
+  ].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "components": path.resolve(__dirname, "./src/components"),
+      "utils": path.resolve(__dirname, "./src/lib/utils"),
+      "ui": path.resolve(__dirname, "./src/components/ui"),
+      "lib": path.resolve(__dirname, "./src/lib"),
+      "hooks": path.resolve(__dirname, "./src/hooks"),
+    },
+  },
+}));
